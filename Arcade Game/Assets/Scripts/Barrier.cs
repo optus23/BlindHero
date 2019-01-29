c82869efb1abb014be67b409247b4e8916bd8d25
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-01-29 15:06:48 +0100

    Set barrier movement && quads movement && quads rebound

---
+++ Arcade Game/Assets/Scripts/Barrier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour {
+
+    public float offset_x = 60;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(gameObject.transform.position.x + offset_x, Input.mousePosition.y, 10));
+	}
+}
---
+++ Arcade Game/Assets/Scripts/SimpleQuads.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleQuads : MonoBehaviour {
+
+    public float velocity_quads = 0.4F;
+    float rebound_velocity_quads;
+    private int state = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rebound_velocity_quads = velocity_quads + 0.2F;
+
+        switch(state)
+        {
+            case 0:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - velocity_quads, gameObject.transform.position.y, 0);
+                break;
+
+            case 1:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + rebound_velocity_quads, gameObject.transform.position.y - rebound_velocity_quads, 0);
+                break;
+            case 2:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + rebound_velocity_quads, gameObject.transform.position.y + rebound_velocity_quads, 0);
+                break;
+
+            default:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - velocity_quads, gameObject.transform.position.y, 0);
+                break;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Hit")
+        {
+            state = Random.Range(1, 3);
+            Debug.Log(state);
+        }
+    }
+}
